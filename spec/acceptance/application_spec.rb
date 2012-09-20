75e23cf565454b3f2442df8a4d5898ca832bdb88
Martin Grenfell <martin.grenfell@gmail.com>
2012-09-20 18:05:27 +0100

    update acceptance test to check a file with 4 errors

--- spec/acceptance/application_spec.rb
+++ spec/acceptance/application_spec.rb
@@ -2,14 +2,27 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 describe "Checking a local file for errors" do
   it "The application checks a file with no errors and reports that it is all good" do
-    $stdout.should_receive(:puts).with(/Syntax OK/)
+    should_output('Syntax OK')
     run_with_test_file("valid.xhtml")
   end
 
-  it "The application finds one error and reports it" do
-    W3Site.any_instance.stub(:results).and_return(open(output_test_file_path('one_error.xhtml')))
-    $stdout.should_receive(:puts).with(/Error: line 9, column 12: end tag for "html" omitted, but OMITTAG NO was specified/)
-    run_with_test_file("one_error.xhtml")
+  it "The application checks a file with errors and reports them" do
+    W3Site.any_instance.stub(:results).and_return(open(output_test_file_path('4_errors.xhtml')))
+
+    should_output_error(9, 21, '"tag" is not a member of a group specified for any attribute')
+    should_output_error(9, 21, 'element "invalid" undefined')
+    should_output_error(10, 19, 'name start character invalid: only S separators and TAGC allowed here')
+    should_output_error(11, 12, 'end tag for "html" omitted, but OMITTAG NO was specified')
+
+    run_with_test_file("4_errors.xhtml")
   end
+end
+
+def should_output_error(line, column, msg)
+  should_output(%Q(Error: line #{line}, column #{column}: #{msg}))
+end
+
+def should_output(msg)
+  $stdout.should_receive(:puts).with(Regexp.new(msg))
 end
 
