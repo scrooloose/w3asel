0a266ea1d807957eb591b027d39a57a9ed1c469f
Martin Grenfell <martin.grenfell@gmail.com>
2012-09-21 10:12:06 +0100

    stub a network call in one of the acceptance tests

--- spec/acceptance/application_spec.rb
+++ spec/acceptance/application_spec.rb
@@ -3,6 +3,8 @@ require File.dirname(__FILE__) + "/../spec_helper"
 describe "Checking a local file for errors" do
   it "The application checks a file with no errors and reports that it is all good" do
     should_output('Syntax OK')
+    W3Site.any_instance.stub(:results).and_return(open(output_test_file_path('valid.xhtml')))
+
     run_with_test_file("valid.xhtml")
   end
 
